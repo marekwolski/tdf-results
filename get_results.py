20e28eeb1f2afd1a693642b4800ca55be811d1dc
marekwolski <marek.wolski@gmail.com>
2024-02-29 18:00:12 +0000

    Early code to extract results

--- get_results.py
+++ get_results.py
@@ -1,13 +1,5 @@
 from openpyxl import load_workbook
-workbook = load_workbook(filename="TdF Stages Riders Results (2011-).xlsx", read_only=True)
-#print(workbook.sheetnames)
-#exit()
-
-#results = workbook['TdF2013 Results']
-#for row in results:
-#    print(row[0].value, row[1].value, row[2].value, row[3].value, row[4].value, row[5].value)
-#    for cell in row:
-#        print(cell.value)
+workbook = load_workbook(filename="TdF Stages Riders Results (2011-).xlsx", read_only=True, data_only=True)
 
 def year_results(year):
     ws = workbook[year]
